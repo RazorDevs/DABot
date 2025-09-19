7d8387ef50f479698e49d56704df50f772bf8f0c
darksonic300 <fabiross096@gmail.com>
2025-09-19 23:01:18 +0100

    setup and build with Shiv

--- dabot.py
+++ dabot.py
@@ -12,10 +12,6 @@ from interactions import check
 from interactions import slash_command, SlashContext, BaseContext
 from playwright.async_api import async_playwright, Playwright, Locator
 
-load_dotenv()
-
-# Initialize Bot and denote the Command Prefix
-bot = AutoShardedClient(intents=Intents.DEFAULT)
 
 # Event when Bot Successfully Connects
 @listen()
@@ -61,100 +57,27 @@ async def on_command_error(ctx, error):
     await ctx.send("Invalid command.")
     print("Invalid Command called.")
 
-# HELP OP COMMAND
-
-@slash_command(name="help", description="List and description of all commands.")
-async def help(ctx: SlashContext):
-    embed = Embed(title="Help", description="Here's the list of all the bots' commands.")
-
-    if is_admin:
-        embed.add_field(name="/purge", value="Removes a max of 10 messages.", inline=False)
-
-    embed.add_field(name="/roll", value="Roll customable dices.", inline=False)
-    embed.add_field(name="/faq", value="Shows our FAQ.", inline=False)
-    await ctx.send(embed=embed)
-
 # Checks if user is an admin
 
 async def is_admin(ctx: BaseContext):
     return ctx.author.guild_permissions.ADMINISTRATOR
 
 
-# Rolls a number of dices.
+def main():
+    # Initialize Bot and denote the Command Prefix
+    bot = AutoShardedClient(intents=Intents.DEFAULT)
 
-@slash_command(name='roll', description='Rolls a max of 5 dices, with a max of 20 faces.')
-@slash_option(
-    	    name="dices",
-	    description="Number of Dices",
-	    opt_type=OptionType.INTEGER,
-	    required=False
-	)
-@slash_option(
-	    name="faces",
-	    description="Number of Faces",
-	    opt_type=OptionType.INTEGER,
-	    required=False
-	)
-async def roll(ctx: SlashContext, dices=1, faces=6):
-    if int(dices) > 5 or int(faces) > 20:
-        print("Roll request exceeds limit.") 
-        await ctx.send("Roll request exceeds limit.")
-    else:
-        rolls = [] 
-        for x in range(int(dices)):
-            rolls.append(random.randint(1, int(faces))) 
-        print(f'Rolled: {rolls};') 
-        await ctx.send(str(rolls))
+    load_dotenv(dotenv_path='./.env')
+    command_list = ["help", "purge", "roll", "faq", "reviews"]
 
-# Clean command for a maximum of 10 messages.
+    for f in command_list:
+        try:
+            exec(compile(open(f"commands/{f}.py", "rb").read(), f"commands/{f}.py", 'exec'))
+        except:
+            print(f"There was an error with the {f} command file.")
 
-@slash_command(name='purge', description="Removes a max of 10 messages.")
-@slash_option(
-	    name="num",
-	    description="Number of messages",
-	    opt_type=OptionType.INTEGER,
-	    required=False
-	)
-@check(is_admin)
-async def purge(ctx: SlashContext, num=1):
-    if num <= 10:
-        num += 1
-        await ctx.channel.purge(deletion_limit=num)
-        num -= 1
-        await ctx.send(f"Removed {num} messages.")
-    else:
-        await ctx.send("That's too many!", ephemeral=True)
-
-# FAQ Command to answer questions. Currently hardcoded
-# TODO: Dynamically retrieve the Q&A (No TXT file)
-
-@slash_command(name='faq', description="Prints the FAQ of Deep Aether.")
-async def faq(ctx: SlashContext):
-    embed = Embed(title="FAQ", color = FlatUIColors.CARROT)
-
-    embed.add_field(name="Q: Do you plan on backporting to other versions?", 
-        value="A: No we don't. The Aether Mod only plans releases from 1.19.2 and onwards, meaning this addon cannot reach versions that are prior to that.", inline=False)
-    embed.add_field(name="Q: Where can I get Sterling Aerclouds? I can't find any!",
-        value="A: Sterling Aerclouds are found above Y = 200. If you're playing with a low render distance it might be harder to spot the clusters, *but they're there*!",
-        inline=False)
-    embed.add_field(name="Q: I'm having trouble finding info about the mod, where can I look for it?",
-        value="A: Unfortunately there's currently no good source of information other than asking us directly. We're working on a wiki hosted on the official Aether one, but we're in desperate need of help!", inline=False)
-    embed.add_field(name="Q: Do you plan on adding cross-compatibility with other mods and Aether addons?", 
-        value="A: Yes, we have already done that with many of the popular addons: Aether Lost Content, Aether Redux, and more!", inline=False)
-    embed.add_field(name="Q: I am interested in joining your team to help with the development of the mod! How can I do so?",
-        value=f"A: We are always open to accepting new members, especially testers, and developers. See {bot.get_channel(1115999673673592832).mention}.", inline=False)
-
-    await ctx.send(embed=embed)
-
-# TODO: Reviews Command
-
-@slash_command(name='review', description="Leave an anonymous review for the mod!")
-async def sendReview(ctx: SlashContext, text):
-    embed = Embed(color = FlatUICOlors.CARROT)
-    ch = bot.get_channel(int(os.getenv('REVIEWS_CHANNEL'))) # Reviews Channel
-    embed.add_field(name="Review", value=text, inline=False)
-    await ctx.send("Review sent!", ephemeral=True)
-    await ch.send(embed=embed)
+    bot.start(os.getenv('TOKEN'))
 
 
-bot.start(os.getenv('TOKEN'))
+if __name__=="__main__":
+    main()
