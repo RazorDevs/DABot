7e9b7e4d75b2b55c286cd2794d19d92448027772
darksonic300 <fabiross096@gmail.com>
2026-04-12 14:19:00 +0100

    Removed stuff

--- build.sh
+++
@@ -1,4 +0,0 @@
-python3 -m venv ./venv
-source venv/bin/activate
-pip install -r requirements.txt
-shiv dabot -o dabot . --python "/usr/bin/python3 -sE"
--- startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /home/admin/Desktop/DiscordBot/dabot/
-source ./venv/bin/activate
-python3 ./dabot.py
