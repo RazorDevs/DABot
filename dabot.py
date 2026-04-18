5359f58106cd9eafb529385f6b3f72569549927a
darksonic300 <fabiross096@gmail.com>
2026-04-19 00:58:06 +0100

    work: changed requests to httpx

--- dabot.py
+++ dabot.py
@@ -2,10 +2,10 @@ import asyncio
 import datetime
 import os
 import random
-import requests
 import discohook
 
 from workers import WorkerEntrypoint, Response
+from httpx import AsyncClient
 
 import commands.help
 import commands.faq
@@ -38,30 +38,30 @@ class Bot(WorkerEntrypoint):
         return await app.handle(request)
 
     async def scheduled(self, event, env):
-        headers = {'Accept': 'application/json', 'x-api-key': env.CURSEFORGE_TOKEN}
+        headers = Headers({'Accept': 'application/json', 'x-api-key': env.CURSEFORGE_TOKEN})
+        async with httpx.AsyncClient() as client:
 
-        async def update_downloads(modid, name, pid, channel_id):
-            CFENDPOINT = f"https://api.curseforge.com/v1/mods/{pid}"
-            MENDPOINT = f"https://api.modrinth.com/v2/project/{modid}"
+            async def update_downloads(modid, name, pid, channel_id):
+                CFENDPOINT = f"https://api.curseforge.com/v1/mods/{pid}"
+                MENDPOINT = f"https://api.modrinth.com/v2/project/{modid}"
 
-            el1 = int(requests.get(CFENDPOINT, headers=headers).json()['data']['downloadCount'])
-            el2 = int(requests.get(MENDPOINT).json()['downloads'])
-            ch = bot.get_channel(channel_id)
+                el1 = int(client.get(CFENDPOINT, headers=headers).json()['data']['downloadCount'])
+                el2 = int(client.get(MENDPOINT).json()['downloads'])
 
-            result = el1 + el2
-            discord_api_url = f"https://discord.com/api/v10/channels/{channel_id}"
-            discord_headers = {
-                "Authorization": f"Bot {env.TOKEN}",
-                "Content-Type": "application/json"
-            }
-            discord_json = {"name": f"{name}:  {result}"}
+                result = el1 + el2
+                discord_api_url = f"https://discord.com/api/v10/channels/{channel_id}"
+                discord_headers = Headers({
+                    "Authorization": f"Bot {env.TOKEN}",
+                    "Content-Type": "application/json"
+                })
+                discord_json = {"name": f"{name}:  {result}"}
 
-            response = requests.patch(discord_api_url, headers=discord_headers, json=discord_json)
-            if response.status_code == 200:
-                print(f"Successfully updated {name} to {result}")
-            else:
-                print(f"Failed to update {name}. Discord replied: {response.text}")
+                response = client.patch(discord_api_url, headers=discord_headers, json=discord_json)
+                if response.status_code == 200:
+                    print(f"Successfully updated {name} to {result}")
+                else:
+                    print(f"Failed to update {name}. Discord replied: {response.text}")
 
-        update_mod_downloads("deep-aether", "Deep Aether", 852465, int(env.DEEP_AETHER_CHANNEL))
-        update_mod_downloads("aeroblender", "Aeroblender", 879879, int(env.AEROBLENDER_CHANNEL))
-        update_mod_downloads("ascended-quark", "Ascended Quark", 971104, int(env.ASCENDED_QUARK_CHANNEL))
+            update_mod_downloads("deep-aether", "Deep Aether", 852465, int(env.DEEP_AETHER_CHANNEL))
+            update_mod_downloads("aeroblender", "Aeroblender", 879879, int(env.AEROBLENDER_CHANNEL))
+            update_mod_downloads("ascended-quark", "Ascended Quark", 971104, int(env.ASCENDED_QUARK_CHANNEL))
