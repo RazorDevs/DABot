2126e3be28e5d110e1f68232feb8688054dbcf8a
darksonic300 <fabiross096@gmail.com>
2026-04-22 01:07:30 +0100

    fixed embed bug

--- dabot.py
+++ dabot.py
@@ -43,9 +43,9 @@ async def faq_cmd(i: discohook.Interaction):
     embed = discohook.Embed(title="FAQ", color=0xe67e22)
 
     embed.add_field(name="Q: Do you plan on backporting to other versions?",
-        value="A: No we don't. The Aether Mod only plans releases from 1.19.2 and onwards, meaning this addon cannot reach versions that are prior to that.", inline=Fa>
+        value="A: No we don't. The Aether Mod only plans releases from 1.19.2 and onwards, meaning this addon cannot reach versions that are prior to that.", inline=False)
     embed.add_field(name="Q: Where can I get Sterling Aerclouds? I can't find any!",
-        value="A: Sterling Aerclouds are found above Y = 200. If you're playing with a low render distance it might be harder to spot the clusters, *but they're there*>
+        value="A: Sterling Aerclouds are found above Y = 200. If you're playing with a low render distance it might be harder to spot the clusters, *but they're there*"
         inline=False)
     embed.add_field(name="Q: I'm having trouble finding info about the mod, where can I look for it?",
         value="A: We finally have some info on the Wiki! Check it out at https://aether.wiki.gg/wiki/Deep_Aether.", inline=False)
@@ -54,7 +54,6 @@ async def faq_cmd(i: discohook.Interaction):
 
     embed.add_field(name="Q: I am interested in joining your team to help with the development of the mod! How can I do so?",
         value="A: We are always open to accepting new members, especially testers, and developers. See <#1115999673673592832>.", inline=False)
-
     await i.response.send(embed=embed)
 
 @discohook.command.slash(name="help", description="List and description of all commands.")
